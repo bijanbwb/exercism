37442c4b6a43138f111a4f96c81f96d6a6a359cd
Bijan <bijanbwb@gmail.com>
2019-09-23 19:54:54 -0400

    (elixir) rna-transcription v2

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -8,11 +8,15 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna), do: Enum.flat_map(dna, &transcribe_nucleotide/1)
+  def to_rna(dna) do
+    dna
+    |> Enum.map(&transcribe_nucleotide/1)
+    |> to_charlist()
+  end
 
   @spec transcribe_nucleotide(char) :: char
-  defp transcribe_nucleotide(?G), do: 'C'
-  defp transcribe_nucleotide(?C), do: 'G'
-  defp transcribe_nucleotide(?T), do: 'A'
-  defp transcribe_nucleotide(?A), do: 'U'
+  defp transcribe_nucleotide(?G), do: ?C
+  defp transcribe_nucleotide(?C), do: ?G
+  defp transcribe_nucleotide(?T), do: ?A
+  defp transcribe_nucleotide(?A), do: ?U
 end
