bedb39b3788df211883efd33ea124ff4b2a1f95c
Bijan <bijanbwb@gmail.com>
2019-09-26 22:40:04 -0400

    (elixir) word-count v3

--- elixir/word-count/lib/words.ex
+++ elixir/word-count/lib/words.ex
@@ -8,7 +8,7 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.split(~r{[\s_:!&@$%^&,]}, trim: true)
+    |> String.split(~r/[^[:alnum:]-]/u, trim: true)
     |> Enum.reduce(%{}, fn word, acc -> Map.update(acc, word, 1, &(&1 + 1)) end)
   end
 end
