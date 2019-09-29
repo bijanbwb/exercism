eca34fc4498a7cb5432944ddd3390480728abd01
Bijan <bijanbwb@gmail.com>
2019-09-29 09:17:57 -0400

    (elixir) roman-numerals v0

---
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,43 @@
+defmodule RomanNumerals do
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    number
+    |> add_tens("")
+  end
+
+  @spec add_ones(pos_integer, String.t()) :: String.t()
+  def add_ones(number, string) when number in 1..3 do
+    Enum.reduce(1..number, string, fn _n, acc -> acc <> "I" end)
+  end
+
+  def add_ones(_number, string) do
+    string
+  end
+
+  @spec add_fives(pos_integer, String.t()) :: String.t()
+  def add_fives(number, string) when number in 4..8 do
+    case number do
+      4 -> string <> "IV"
+      _5 -> string <> "V" <> add_ones(rem(number, 5), string)
+    end
+  end
+
+  def add_fives(number, string) do
+    add_ones(number, string)
+  end
+
+  @spec add_tens(pos_integer, String.t()) :: String.t()
+  def add_tens(number, string) when number in 9..13 do
+    case number do
+      9 -> string <> "IX"
+      _10 -> string <> "X" <> add_fives(rem(number, 10), string)
+    end
+  end
+
+  def add_tens(number, string) do
+    add_fives(number, string)
+  end
+end
