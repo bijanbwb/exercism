6ed4ce328280218b49d4c183e391166ffe299f1b
Bijan <bijanbwb@gmail.com>
2019-09-29 21:08:51 -0400

    (javascript) resistor-color-duo v2

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,22 +1,2 @@
-let resistanceValues = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
-}
-
-export const value = (input) => {
-  let resistance =
-    input
-      .slice(0, 2)
-      .map(color => String(resistanceValues[color]))
-      .reduceRight((string, acc) => acc + string);
-
-  return Number(resistance);
-};
+const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+export const value = (input) => Number(`${colors.indexOf(input[0])}${colors.indexOf(input[1])}`);
