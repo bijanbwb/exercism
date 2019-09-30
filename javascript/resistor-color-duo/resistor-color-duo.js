7f6a2a6ae92e10330d20de3acfafcf07fc8e7edc
Bijan <bijanbwb@gmail.com>
2019-09-29 23:44:24 -0400

    (javascript) resistor-color-duo v3

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,2 +1,2 @@
 const colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-export const value = (input) => Number(`${colors.indexOf(input[0])}${colors.indexOf(input[1])}`);
+export const value = ([first, second]) => colors.indexOf(first) * 10 + colors.indexOf(second);
