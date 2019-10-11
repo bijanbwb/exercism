5578174b86ecd0c58d493defb6dc8770d8d7bf6e
Bijan <bijanbwb@gmail.com>
2019-10-11 16:05:44 -0400

    (elixir) robot-simulator v5

--- elixir/robot-simulator/lib/robot_simulator.ex
+++ elixir/robot-simulator/lib/robot_simulator.ex
@@ -4,26 +4,24 @@ defmodule Robot do
 end
 
 defmodule RobotSimulator do
-  alias Robot
-
-  # Types
-
   @type direction :: :north | :east | :south | :west
+  @type instruction :: ?A | ?L | ?R
   @type position :: {integer, integer}
   @type robot :: %Robot{direction: direction(), position: position()}
 
-  # Valid Options
-
   @valid_directions [:north, :east, :south, :west]
-  @valid_instructions ["A", "L", "R"]
+  @valid_instructions [?A, ?L, ?R]
+
+  defguard is_direction(direction) when direction in @valid_directions
+  defguard is_instruction(instruction) when instruction in @valid_instructions
 
   @doc """
   Create a Robot Simulator given an initial direction and position.
   """
-  @spec create(direction :: direction(), position :: position()) :: robot() | {:error, String.t()}
+  @spec create(direction(), position()) :: robot() | {:error, String.t()}
   def create(direction \\ :north, position \\ {0, 0})
 
-  def create(direction, _position) when direction not in @valid_directions do
+  def create(direction, _position) when not is_direction(direction) do
     {:error, "invalid direction"}
   end
 
@@ -43,9 +41,9 @@ defmodule RobotSimulator do
 
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
-  @spec simulate(robot :: robot(), instructions :: String.t()) :: robot()
-  def simulate(robot, instructions) do
-    instruction_list = String.graphemes(instructions)
+  @spec simulate(robot(), String.t()) :: robot()
+  def simulate(%Robot{} = robot, instructions) do
+    instruction_list = String.to_charlist(instructions)
 
     instruction_list
     |> Enum.all?(&(&1 in @valid_instructions))
@@ -55,34 +53,25 @@ defmodule RobotSimulator do
     end
   end
 
-  @spec run_simulation_step(letter :: String.t(), robot :: robot()) :: robot()
-  def run_simulation_step(letter, robot) do
-    case letter do
-      "A" -> %Robot{robot | position: change_position(robot)}
-      "L" -> %Robot{robot | direction: change_direction(robot, "L")}
-      "R" -> %Robot{robot | direction: change_direction(robot, "R")}
-    end
+  @spec run_simulation_step(instruction(), robot()) :: robot()
+  def run_simulation_step(?A, %Robot{} = robot),
+    do: %Robot{robot | position: change_position(robot)}
+
+  def run_simulation_step(letter, %Robot{} = robot) do
+    %Robot{robot | direction: change_direction(robot, letter)}
   end
 
-  @spec change_direction(robot :: robot(), letter :: String.t()) :: direction()
-  def change_direction(%Robot{direction: direction}, "L") do
-    @valid_directions
-    |> Enum.reverse()
-    |> Stream.cycle()
-    |> Stream.drop_while(&(&1 != direction))
-    |> Enum.take(2)
-    |> Enum.at(1)
-  end
+  @spec change_direction(robot(), instruction()) :: direction()
+  def change_direction(%Robot{direction: :north}, ?L), do: :west
+  def change_direction(%Robot{direction: :east}, ?L), do: :north
+  def change_direction(%Robot{direction: :south}, ?L), do: :east
+  def change_direction(%Robot{direction: :west}, ?L), do: :south
+  def change_direction(%Robot{direction: :north}, ?R), do: :east
+  def change_direction(%Robot{direction: :east}, ?R), do: :south
+  def change_direction(%Robot{direction: :south}, ?R), do: :west
+  def change_direction(%Robot{direction: :west}, ?R), do: :north
 
-  def change_direction(%Robot{direction: direction}, "R") do
-    @valid_directions
-    |> Stream.cycle()
-    |> Stream.drop_while(&(&1 != direction))
-    |> Enum.take(2)
-    |> Enum.at(1)
-  end
-
-  @spec change_position(robot :: robot()) :: position()
+  @spec change_position(robot()) :: position()
   def change_position(%Robot{direction: :north, position: {x, y}}), do: {x, y + 1}
   def change_position(%Robot{direction: :east, position: {x, y}}), do: {x + 1, y}
   def change_position(%Robot{direction: :south, position: {x, y}}), do: {x, y - 1}
@@ -91,15 +80,15 @@ defmodule RobotSimulator do
   @doc """
   Return the robot's direction.
   """
-  @spec direction(robot :: robot()) :: direction()
-  def direction(%{direction: :north}), do: :north
-  def direction(%{direction: :east}), do: :east
-  def direction(%{direction: :south}), do: :south
-  def direction(%{direction: :west}), do: :west
+  @spec direction(robot()) :: direction()
+  def direction(%Robot{direction: :north}), do: :north
+  def direction(%Robot{direction: :east}), do: :east
+  def direction(%Robot{direction: :south}), do: :south
+  def direction(%Robot{direction: :west}), do: :west
 
   @doc """
   Return the robot's position.
   """
-  @spec position(robot :: robot()) :: position()
-  def position(%{position: {x, y}}), do: {x, y}
+  @spec position(robot()) :: position()
+  def position(%Robot{position: {x, y}}), do: {x, y}
 end
