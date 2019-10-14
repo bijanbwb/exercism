b0e9a773dce20fefc8f4d2c1c0698af2111160e7
Bijan <bijanbwb@gmail.com>
2019-10-14 09:12:26 -0400

    (elixir) list-ops v0

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -1,36 +1,34 @@
 defmodule ListOps do
-  # Please don't use any external modules (especially List or Enum) in your
-  # implementation. The point of this exercise is to create these basic
-  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
-  # for adding numbers), but please do not use Kernel functions for Lists like
-  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+  @spec count(list()) :: non_neg_integer()
+  def count([_head | tail]), do: 1 + count(tail)
+  def count([]), do: 0
 
-  @spec count(list) :: non_neg_integer
-  def count(l) do
+  @spec reverse(list()) :: list()
+  def reverse([head | tail]), do: reverse(tail) ++ [head]
+  def reverse([]), do: []
+
+  @spec map(list(), (any() -> any())) :: list()
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
+  def map([], _f), do: []
+
+  @spec filter(list(), (any() -> as_boolean(term))) :: list()
+  def filter([head | tail], f) do
+    if(f.(head), do: [head | filter(tail, f)], else: filter(tail, f))
   end
 
-  @spec reverse(list) :: list
-  def reverse(l) do
-  end
+  def filter([], _f), do: []
 
-  @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-  end
+  @type acc :: any()
+  @spec reduce(list(), acc(), (any(), acc() -> acc())) :: acc()
+  def reduce([head | tail], acc, f), do: reduce(tail, f.(head, acc), f)
+  def reduce([], acc, _f), do: acc
 
-  @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-  end
-
-  @type acc :: any
-  @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
-  end
-
-  @spec append(list, list) :: list
+  @spec append(list(), list()) :: list()
   def append(a, b) do
+    a ++ b
   end
 
-  @spec concat([[any]]) :: [any]
-  def concat(ll) do
-  end
+  @spec concat([[any()]]) :: [any()]
+  def concat([head | tail]), do: head ++ concat(tail)
+  def concat([]), do: []
 end
