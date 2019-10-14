f10e9e059ade3cf3b275aebd96d5a5b2272964be
Bijan <bijanbwb@gmail.com>
2019-10-14 09:28:30 -0400

    (elixir) list-ops v0

--- elixir/list-ops/lib/list_ops.ex
+++ elixir/list-ops/lib/list_ops.ex
@@ -1,34 +1,33 @@
 defmodule ListOps do
   @spec count(list()) :: non_neg_integer()
-  def count([_head | tail]), do: 1 + count(tail)
   def count([]), do: 0
+  def count([_head | tail]), do: 1 + count(tail)
 
   @spec reverse(list()) :: list()
-  def reverse([head | tail]), do: reverse(tail) ++ [head]
   def reverse([]), do: []
+  def reverse([head | tail]), do: reverse(tail) ++ [head]
 
   @spec map(list(), (any() -> any())) :: list()
-  def map([head | tail], f), do: [f.(head) | map(tail, f)]
   def map([], _f), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list(), (any() -> as_boolean(term))) :: list()
-  def filter([head | tail], f) do
-    if(f.(head), do: [head | filter(tail, f)], else: filter(tail, f))
-  end
-
   def filter([], _f), do: []
+
+  def filter([head | tail], f) do
+    if f.(head), do: [head | filter(tail, f)], else: filter(tail, f)
+  end
 
   @type acc :: any()
   @spec reduce(list(), acc(), (any(), acc() -> acc())) :: acc()
-  def reduce([head | tail], acc, f), do: reduce(tail, f.(head, acc), f)
   def reduce([], acc, _f), do: acc
+  def reduce([head | tail], acc, f), do: reduce(tail, f.(head, acc), f)
 
   @spec append(list(), list()) :: list()
-  def append(a, b) do
-    a ++ b
-  end
+  def append([], []), do: []
+  def append(a, b), do: a ++ b
 
   @spec concat([[any()]]) :: [any()]
-  def concat([head | tail]), do: head ++ concat(tail)
   def concat([]), do: []
+  def concat([head | tail]), do: head ++ concat(tail)
 end
